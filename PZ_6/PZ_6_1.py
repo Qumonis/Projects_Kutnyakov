2320b0486d758beb3b5f8e3c44662003e8120fd5
Qumonis <kutnakovdaniil01@gmail.com>
2025-12-12 21:53:20 +0300

    PZ_6

---
+++ PZ_6/PZ_6_1.py
@@ -0,0 +1,38 @@
+#Дан список размера N и целые числа K и L (1 < K < L < N). Найти сумму элементов
+#списка с номерами от K до L включительно.
+import random
+k, l, n = input('k > 1: '), input('l > k: '), input('n > l: ')
+
+while type(n) != int:  # Обработка исключений
+    try:
+        n = int(n)
+    except ValueError:
+        print('Не правильно ввёл')
+        n = input('n:')
+
+while type(k) != int:  # Обработка исключений
+    try:
+        k = int(k)
+    except ValueError:
+        print('Не правильно ввёл')
+        k = input('k:')
+
+while type(l) != int:  # Обработка исключений
+    try:
+        l = int(l)
+    except ValueError:
+        print('Не правильно ввёл')
+        l = input('l:')
+
+if 1 > k or k > l or l > n:
+    print('Заного')
+else:
+    c = []
+    f = 0
+    for i in range(n):
+        i += 1
+        c.append(random.randint(0, 5))
+    b = c[c.index(k):c.index(l) + 1]
+    b = sum(b)
+    print(c)
+    print(b)
