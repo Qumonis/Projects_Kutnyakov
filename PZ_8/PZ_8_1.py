f0ea1db33d120c0c89da5466349bc238cfb9f3da
Qumonis <kutnakovdaniil01@gmail.com>
2026-01-31 12:45:57 +0300

    PZ_8

--- PZ_8/PZ_8_1.py
+++ PZ_8/PZ_8_1.py
@@ -1,6 +1,7 @@
 #Выведите все ключи и значения через цикл.
 #d = {'Red':1, 'Green':2}
 d = {'Red':1, 'Green':2}
-for i in d.keys():
-    print(i)
+for i in d.items():
+    for j in i:
+        print(j)
 print(d)
